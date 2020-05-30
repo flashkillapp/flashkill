7484ba112e181b0737152bf68685b424171e07d2
Timon Lapawczyk <timon.lapawczyk@gmail.com>
2020-05-30 14:19:46 +0200

    Make team from team page always home team

--- src/scripts/team-page/seasons/content.js
+++ src/scripts/team-page/seasons/content.js
@@ -1,3 +1,4 @@
+
 const mapImageInferno = "https://cdn0.gamesports.net/map_thumbs_big/90.png?1487240877";
 const mapImageMirage = "https://cdn0.gamesports.net/map_thumbs_big/94.jpg?1543845869";
 const mapImageDust2 = "https://cdn0.gamesports.net/map_thumbs_big/88.png?1525340267";
@@ -48,16 +49,20 @@ function insertMatchResults() {
         { contentScriptQuery: "apiMatches", teamId },
         matches => {
             for (matchEntry in matches) {
-                const match = matches[matchEntry];
+                let match = matches[matchEntry];
+                if (match.team1 == undefined) {
+                    continue;
+                }
+                match = makeHomeMatch(match, teamId);
                 const season = seasons.find(season => season.id == match.season)
                 if (season != undefined) season.matches.push(match);
             }
-            injectSeasons(seasons);
+            insertSeasons(seasons);
         }
     );
 }
 
-function injectSeasons(seasons) {
+function insertSeasons(seasons) {
     const activeSeasons = Array.from(seasons).filter(season => season.matches.length > 0);
 
     const parentDiv = document.querySelector("#container > main > section.boxed-section.hybrid");
@@ -67,12 +72,12 @@ function injectSeasons(seasons) {
 
     if (activeSeasons.length > 0) {
         activeSeasons.forEach(season => {
-            insertResults(season);
+            insertSeasonResults(season);
         });
     }
 }
 
-function insertResults(season) {
+function insertSeasonResults(season) {
     const dividerDiv = document.createElement("div");
     const dividerBr = document.createElement("br");
     dividerDiv.appendChild(dividerBr);
@@ -107,13 +112,13 @@ function insertResults(season) {
 
     const body = table.createTBody();
     matches.forEach(match => {
-       if (match.maps.length > 0) {
-           match.maps.forEach(map => {
-               var row = body.insertRow(0);
-               console.log(match, match.team1, match.team2);
-               fillMapRow(row, match.id, map, match.team1, match.team2, match.createdAt);
-           });
-       }
+        if (match.maps.length > 0) {
+            match.maps.forEach(map => {
+                var row = body.insertRow(0);
+                console.log(match, match.team1, match.team2);
+                fillMapRow(row, match.id, map, match.team1, match.team2, match.createdAt);
+            });
+        }
     });
 
     seasonDiv.appendChild(table);
@@ -151,29 +156,6 @@ function insertResults(season) {
             $(row).find('td:eq(3)').css('backgroundColor', columnColor);
         }
     });
-}
-
-function buildLinkInformation(seasonTd) {
-    const links = seasonTd.getElementsByTagName("a");
-    if (links.length == 2) {
-        return {
-            seasonUrl: links[0].href,
-            additionalUrl: links[1].href,
-            additionalInfo: links[1].textContent
-        }
-    }
-    return {
-        seasonUrl: links[0].href
-    }
-}
-
-function formatDate(date) {
-    //Gets date in yyyy-mm-dd format
-    //Returns date in dd.mm.yy format
-    const year = date.substring(2, 4);
-    const month = date.substring(5, 7);
-    const day = date.substring(8, 10);
-    return day + "." + month + "." + year;
 }
 
 function fillMapRow(row, matchId, map, team1, team2, matchDate) {
@@ -214,35 +196,16 @@ function fillMapRow(row, matchId, map, team1, team2, matchDate) {
     linkC.appendChild(linkA);
 }
 
-function getMapImage(mapName) {
-    switch (mapName) {
-        case "de_inferno":
-            return mapImageInferno;
-        case "de_mirage":
-            return mapImageMirage;
-        case "de_dust2":
-            return mapImageDust2;
-        case "de_cache":
-            return mapImageCache;
-        case "de_cbble":
-            return mapImageCobblestone;
-        case "de_train":
-            return mapImageTrain;
-        case "de_overpass":
-            return mapImageOverpass;
-        case "de_vertigo":
-            return mapImageVertigo;
-        case "de_nuke":
-            return mapImageNuke;
-        default:
-            return "";
+function makeHomeMatch(match, teamId) {
+    if (match.team1.id != teamId) {
+        const tmpTeam = match.team1;
+        match.team1 = match.team2;
+        match.team2 = tmpTeam;
+        match.maps.forEach(map => {
+            const tmpScore = map.score1;
+            map.score1 = map.score2;
+            map.score2 = tmpScore;
+        });
     }
-}
-
-function insertDataTablesCss() {
-    const datatablescss = document.createElement("link");
-    datatablescss.setAttribute("rel", "stylesheet");
-    datatablescss.setAttribute("type", "text/css");
-    datatablescss.setAttribute("href", "https://cdn.datatables.net/v/dt/jq-3.3.1/dt-1.10.18/datatables.min.css");
-    document.body.appendChild(datatablescss);
+    return match;
 }

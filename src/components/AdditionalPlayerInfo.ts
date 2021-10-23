104fb8fd7e3d488c1b867257bd81928a094ccb7b
Timon Lapawczyk <timon.lapawczyk@gmail.com>
2021-10-23 12:25:19 +0200

    Only call faceitInfo renderer if not undefined

--- src/components/AdditionalPlayerInfo.ts
+++ src/components/AdditionalPlayerInfo.ts
@@ -2,9 +2,8 @@ import '@webcomponents/custom-elements';
 import { LitElement, css, html, HTMLTemplateResult } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 
-import { PlayerInfo } from '../model';
+import { FaceitInfo, PlayerInfo } from '../model';
 import { getSteamLink } from '../util/getSteamLink';
-import { isUndefined } from '../util';
 
 const getFaceitLevelLogo = (faceitLevel: number): string => (
   `https://cdn-frontend.faceit.com/web/960/src/app/assets/images-compress/skill-icons/skill_level_${faceitLevel}_svg.svg`
@@ -63,13 +62,11 @@ class AdditionalPlayerInfo extends LitElement {
     }
   `;
 
-  private renderFaceitInfo(): HTMLTemplateResult | null {
-    if (isUndefined(this.playerInfo.faceitInfo)) return null;
-
+  private renderFaceitInfo(faceitInfo: FaceitInfo): HTMLTemplateResult {
     const {
       nickname,
       games: { csgo: { faceit_elo, skill_level } },
-    } = this.playerInfo.faceitInfo;
+    } = faceitInfo;
 
     return html`
       <div class="faceit-info">
@@ -85,15 +82,17 @@ class AdditionalPlayerInfo extends LitElement {
   }
 
   render() {
+    const { steamId64, steamName, faceitInfo } = this.playerInfo;
+
     return html`
       <div class="root">
         <div class="steam-info">
           ${'Steam: '}
-          <a href=${getSteamLink(this.playerInfo.steamId64)} target="_blank">
-            ${this.playerInfo.steamName ?? '-'}
+          <a href=${getSteamLink(steamId64)} target="_blank">
+            ${steamName ?? '-'}
           </a>
         </div>
-        ${this.renderFaceitInfo()}
+        ${faceitInfo && this.renderFaceitInfo(faceitInfo)}
       </div>
     `;
   }

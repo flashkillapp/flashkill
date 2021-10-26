ccc6d712ccf2df273f98d3aa112da2eaa461b913
Timon Lapawczyk <timon.lapawczyk@gmail.com>
2021-10-26 07:46:18 +0200

    Rename prefixZero function

--- src/util/dateHelpers.ts
+++ src/util/dateHelpers.ts
@@ -1,4 +1,4 @@
-const prefixZero = (i: number): string => (
+const makeDoubleDigit = (i: number): string => (
   i < 10 ? `0${i}` : `${i}`
 );
 
@@ -12,11 +12,11 @@ interface DateUnits {
 
 export const getUnits = (time: number): DateUnits => {
   const date = new Date(time * 1000);
-  const year = prefixZero(date.getFullYear());
-  const month = prefixZero(date.getMonth() + 1);
-  const day = prefixZero(date.getDate());
-  const hours = prefixZero(date.getHours());
-  const minutes = prefixZero(date.getMinutes());
+  const year = makeDoubleDigit(date.getFullYear());
+  const month = makeDoubleDigit(date.getMonth() + 1);
+  const day = makeDoubleDigit(date.getDate());
+  const hours = makeDoubleDigit(date.getHours());
+  const minutes = makeDoubleDigit(date.getMinutes());
 
   return {
     year, month, day, hours, minutes,

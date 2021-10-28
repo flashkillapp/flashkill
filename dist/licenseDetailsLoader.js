40a49405d593a0d28bf50afabb12a7df4aac041c
Timon Lapawczyk <timon.lapawczyk@gmail.com>
2021-10-28 23:08:29 +0200

    Update logos and popup (#65)
    * Update icons
    
    * Show entire popup in firefox
    
    * Remove unused styles and style buttons

--- dist/licenseDetailsLoader.js
+++
@@ -1,2 +0,0 @@
-const url = chrome.runtime.getURL('LICENSE-DETAILS.md');
-document.getElementById('license-link').href = url;
--- dist/popup.js
+++ dist/popup.js
@@ -1,3 +1,7 @@
 const clearCache = () => chrome.storage.local.clear(() => alert('Cache wurde geleert.'));
 
 document.getElementById('clear-cache-button').onclick = clearCache;
+
+document.getElementById('flashkill-logo').src = chrome.runtime.getURL('icons/flashkill-logo-white.png');
+document.getElementById('donation-button').src = chrome.runtime.getURL('icons/donation-button.png');
+document.getElementById('license-link').href = chrome.runtime.getURL('LICENSE-DETAILS.md');

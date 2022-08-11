01231029d54abb30f4106a0ad52fffc2fd921de3
Timon Lapawczyk <timon.lapawczyk@gmail.com>
2022-08-11 08:28:26 +0200

    Update to new 99 damage URL

--- src/util/getLink.ts
+++ src/util/getLink.ts
@@ -1,17 +1,19 @@
+export const baseURL99 = 'https://liga.esl-meisterschaft.de';
+
 export const getSteamLink = (steamId64: string): string => (
   `https://steamcommunity.com/profiles/${steamId64}`
 );
 
 export const get99MatchLink = (matchId: number): string => (
-  `https://liga.99damage.de/leagues/matches/${matchId}`
+  `${baseURL99}/leagues/matches/${matchId}`
 );
 
 export const get99TeamLink = (teamId: number): string => (
-  `https://liga.99damage.de/leagues/teams/${teamId}`
+  `${baseURL99}/leagues/teams/${teamId}`
 );
 
 export const get99PlayerLink = (playerId: number): string => (
-  `https://liga.99damage.de/users/${playerId}`
+  `${baseURL99}/users/${playerId}`
 );
 
 export const getSteamLogoLink = (): string => (
